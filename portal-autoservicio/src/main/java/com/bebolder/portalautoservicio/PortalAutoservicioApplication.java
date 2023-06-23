0ceff0a6bd07c0d04f3857a4604542f5679dfce1
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-06-23 17:43:55 -0500

    Creacion del proyecto API Portal Autoservicio con spring initializr

---
+++ portal-autoservicio/src/main/java/com/bebolder/portalautoservicio/PortalAutoservicioApplication.java
@@ -0,0 +1,13 @@
+package com.bebolder.portalautoservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAutoservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAutoservicioApplication.class, args);
+	}
+
+}
