acd13d2e34a014eede0e992506ccf8c747c848ef
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-06-27 17:21:50 -0500

    Creacion de la capa controller para el dominio de administrador - Tengo que modificar las BD que tenia, ya que aplique la recomendaciones del entregable 4 de BD

---
+++ portal-autoservicio/src/main/java/com/bebolder/portalautoservicio/web/controller/AdministradorDtoController.java
@@ -0,0 +1,30 @@
+package com.bebolder.portalautoservicio.web.controller;
+
+import com.bebolder.portalautoservicio.domain.dto.AdministradorDto;
+import com.bebolder.portalautoservicio.domain.service.AdministradorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/administrador")
+public class AdministradorDtoController {
+
+    @Autowired
+    private AdministradorService administradorService;
+
+    public List<AdministradorDto> getAll() {
+        return administradorService.getAll();
+    }
+
+    public Optional<List<AdministradorDto>> getUserByRol(int rolId) {
+        return administradorService.getUserByRol(rolId);
+    }
+
+    public AdministradorDto save(AdministradorDto administradorDto) {
+        return administradorService.save(administradorDto);
+    }
+}
