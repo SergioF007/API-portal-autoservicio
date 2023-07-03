af58d959babbafc9d9b4c8621507863029749b38
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-07-03 15:57:30 -0500

    Validacion solicitud de vacaciones - La solicitud de vacaciones debe ser  15 dias antes del inicio de las vacaciones - Prueba Exitosa - no permite la creacion de la solicitud y muestra el mensaje del no cumplimiento de la condicion

--- portal-autoservicio/src/main/java/com/bebolder/portalautoservicio/domain/service/SolicitudVacacionesService.java
+++ portal-autoservicio/src/main/java/com/bebolder/portalautoservicio/domain/service/SolicitudVacacionesService.java
@@ -5,6 +5,8 @@ import com.bebolder.portalautoservicio.domain.repository.SolicitudVacacionesDtoR
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,8 +29,34 @@ public class SolicitudVacacionesService {
 
     public SolicitudVacacionesDto save(SolicitudVacacionesDto solicitudVacacionesDto) {
 
-        return solicitudVacacionesDtoRepository.save(solicitudVacacionesDto);
+        // Validar la solicitud de vacaciones con las reglas de negocio
+
+        validarFechaInicio(solicitudVacacionesDto);
+
+
+        //si todas las validaciones pasan se procede a guardar la solicitud de vacaciones
+        try {
+            return solicitudVacacionesDtoRepository.save(solicitudVacacionesDto);
+
+        } catch (Exception e) {
+            throw new ValidacionException("Error al guardar la solicitud de vacaciones");
+        }
     }
 
+    private void validarFechaInicio(SolicitudVacacionesDto solicitudVacacionesDto) {
+        LocalDate fechaCreacion = LocalDate.now();
+        LocalDate fechaInicio = solicitudVacacionesDto.getInicioVacaciones();
+
+        long diasDiferencia = ChronoUnit.DAYS.between(fechaCreacion, fechaInicio);
+        if (diasDiferencia <= 15) {
+            throw new ValidacionException("La fecha de inicio de las vacaciones debe ser al menos 15 dias despues de la fecha de creación de la solicitud.");
+        }
+    }
+
+    public class ValidacionException extends RuntimeException {
+        public ValidacionException(String mensaje) {
+            super(mensaje);
+        }
+    }
 
 }
